57ba17d1e87f6c64eb16a257ee80af49cb3b17ed
Tor <tornilssonohrn@gmail.com>
2013-12-18 11:09:27 +0100

    scaffolding

---
+++ TIMELOG.md
@@ -0,0 +1,5 @@
+## Timelog for neon
+* 2013-12-11 started reading about distance transform methods 1 hrs 7 min
+* 2013-12-18 read about DT and set up WebGl project 4 hrs 37 min
+
+### Total time spent: 5 hours and 44 minutes 
---
+++ neon.js
@@ -0,0 +1,148 @@
+var gl;
+function initGL(canvas) {
+	try {
+		gl = canvas.getContext("webgl");
+		gl.viewportWidth = canvas.width;
+		gl.viewportHeight = canvas.height;
+	} catch (e) {
+	}
+	if (!gl) {
+		alert("Could not initialise WebGL, sorry :-(");
+	}
+}
+
+function getShader(gl, id) {
+
+	var shaderScript = document.getElementById(id)
+
+	// read content of shaderfile into string
+	var request = new XMLHttpRequest();
+	request.open("GET", shaderScript.src, false);
+	request.send(null);
+	var returnValue = request.responseText;
+
+	if (!returnValue) {
+		return null;
+	}
+
+	var str = returnValue;
+
+	var shader;
+	if (shaderScript.type == "x-shader/x-fragment") {
+		shader = gl.createShader(gl.FRAGMENT_SHADER);
+	} else if (shaderScript.type == "x-shader/x-vertex") {
+		shader = gl.createShader(gl.VERTEX_SHADER);
+	} else {
+		return null;
+	}
+
+	gl.shaderSource(shader, str);
+	gl.compileShader(shader);
+
+	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+		alert(gl.getShaderInfoLog(shader));
+		return null;
+	}
+
+	return shader;
+}
+
+
+var shaderProgram;
+
+function initShaders() {
+	var fragmentShader = getShader(gl, "shader-fs");
+	var vertexShader = getShader(gl, "shader-vs");
+
+	shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vertexShader);
+	gl.attachShader(shaderProgram, fragmentShader);
+	gl.linkProgram(shaderProgram);
+
+	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+		alert("Could not initialise shaders");
+	}
+
+	gl.useProgram(shaderProgram);
+
+	shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+	gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+	shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+	shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+}
+
+
+var mvMatrix = mat4.create();
+var pMatrix = mat4.create();
+
+function setMatrixUniforms() {
+	gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+	gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+}
+
+
+
+// var triangleVertexPositionBuffer;
+// var squareVertexPositionBuffer;
+
+// function initBuffers() {
+
+// 	triangleVertexPositionBuffer = gl.createBuffer();
+// 	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+// 	var vertices = [
+// 		 0.0,  1.0,  0.0,
+// 		-1.0, -1.0,  0.0,
+// 		 1.0, -1.0,  0.0
+// 	];
+// 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+// 	triangleVertexPositionBuffer.itemSize = 3;
+// 	triangleVertexPositionBuffer.numItems = 3;
+
+// }
+
+
+function drawScene() {
+	gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+
+	mat4.identity(mvMatrix);
+
+	// animate rotation
+	currentAngle += incAngle;
+	if (currentAngle > 2*Math.PI) {
+		currentAngle -= 2*Math.PI;
+	}
+
+	mvMatrixTrsanslated = mat4.create();
+	mat4.translate(mvMatrixTrsanslated, mvMatrix, [0.0, 0.0, -3.0]);
+	mvMatrixRotated = mat4.create();
+	mat4.rotateY(mvMatrixRotated, mvMatrix, currentAngle);
+
+	mvMatrix = mat4.clone(mvMatrixRotated);
+
+	// gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+	// gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	setMatrixUniforms();
+	// gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
+
+	requestAnimationFrame(drawScene);
+}
+
+
+
+function webGLStart() {
+	var canvas = document.getElementById("neon-canvas");
+	initGL(canvas);
+	initShaders();
+	// initBuffers();
+
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	gl.enable(gl.DEPTH_TEST);
+
+	currentAngle = 0;
+	incAngle = 1/(20*Math.PI);
+	drawScene();
+}
